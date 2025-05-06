f593cbfcabefb2c32bd141dc37bd4b5ffad6c543
fabio ellena <ellena.fabio@gmail.com>
2025-05-06 22:33:11 +0200

    add integration test

---
+++ starter/integration_test.py
@@ -0,0 +1,24 @@
+import json
+import requests
+
+data = {
+    "age": 50,
+    "workclass": "Self-emp-not-inc",
+    "fnlgt": 83311,
+    "education": "HS-grad",
+    "education_num": 9,
+    "marital-status": "Married-civ-spouse",
+    "occupation": "Craft-repair",
+    "relationship": "Husband",
+    "race": "White",
+    "sex": "Male",
+    "capital_gain": 0,
+    "capital_loss": 0,
+    "hours_per_week": 40,
+    "native-country": "United-States"
+}
+
+response = requests.post('http://127.0.0.1:8000/predict', data=json.dumps(data))
+
+print(response.status_code)
+print(response.json())
